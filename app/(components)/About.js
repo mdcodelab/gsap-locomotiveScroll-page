cd57e2797095fb317b79a5cd799b6be5a66019a9
miha11-hash <d_mihaela@msn.com>
2025-01-22 00:20:53 +0200

    About

--- app/(components)/About.js
+++ app/(components)/About.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import SectionHeader from './SectionHeader';
 
 function About() {
   return (
-    <div>
-      About
-    </div>
+    <section className="about-section">
+    <SectionHeader title="About"></SectionHeader>
+      <p className="headline">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et 
+        voluptatibus eius qui explicabo, aperiam facere culpa sint eos 
+        ratione quasi at accusantium, omnis, tempore delectus incidunt 
+        quos veritatis perferendis ad consequatur veniam. Ratione deleniti 
+        dignissimos soluta, quidem dolores, tempora ipsa qui nihil facere
+         voluptatem id. Aliquid ab dicta architecto laboriosam.
+      </p>
+    </section>
   )
 }
 
---
+++ app/(components)/SectionHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SectionHeader({title}) {
+  return (
+    <div className="section-header">
+      {title}
+    </div>
+  )
+}
+
+export default SectionHeader
