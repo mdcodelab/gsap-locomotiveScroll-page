0c120ceb121534ea1a2568981c2f5f542d237809
miha11-hash <d_mihaela@msn.com>
2025-01-23 16:08:01 +0200

    gsap text animation

--- app/(components)/Header.js
+++ app/(components)/Header.js
@@ -1,6 +1,30 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useRef, useLayoutEffect } from 'react';
+import gsap from 'gsap';
 
 function Header() {
+  const ref_1 = useRef(null);
+  const ref_2 = useRef(null);
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const t1 = gsap.timeline();
+      t1.from(ref_1.current, {
+        y: "+=30",
+        duration: 0.3,
+        opacity: 0
+      })
+        .from(ref_2.current, {
+          opacity: 0,
+          y: "+=30",
+          delay: 0.3,
+          stagger: 0.5,
+        });
+    });
+
+    return () => ctx.revert(); // Cleanup pentru GSAP context
+  }, []);
+
   return (
     <section className="header-container" data-scroll-section>
       <ul className="header-menu">
@@ -8,9 +32,11 @@ function Header() {
         <li>About</li>
         <li>Featured</li>
       </ul>
-      <h1 id="header-text">Art Objects</h1>
+      <h1 id="header-text" style={{ position: "relative" }}>
+        <span ref={ref_1}>Art</span> <span ref={ref_2}>Object</span>
+      </h1>
     </section>
-  )
+  );
 }
 
-export default Header
+export default Header;
