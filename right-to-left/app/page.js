b3e8ba1c9ce07412842b376787db2ec86d5290b1
miha11-hash <d_mihaela@msn.com>
2025-01-24 13:16:59 +0200

    restructure files

---
+++ right-to-left/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RightToLeft from "./components)/RightToLeft"
+
+function page() {
+  return (
+    <div>
+      <RightToLeft></RightToLeft>
+    </div>
+  )
+}
+
+export default page
